e50d9fff7bd441b232fa0ad3fc59d368360a1fef
rgdl <robert_delisle44@hotmail.com>
2020-06-12 20:58:15 +1000

    draw all gridlines

--- src/display.rs
+++ src/display.rs
@@ -1,6 +1,6 @@
 use glium::{glutin, Surface};
 
-pub struct Display { columns: i32, rows: i32, }
+pub struct Display { columns: i32, rows: i32 }
 
 struct Colour {
     red: f32,
@@ -20,12 +20,48 @@ static GRIDLINES: Colour = Colour { red: 0.0, green: 1.0, blue: 0.0 };
 static CELL: Colour = Colour { red: 1.0, green: 0.0, blue: 0.0 };
 
 static WINDOW_TITLE: &str = "Game of Life";
-static WINDOW_HEIGHT: u32 = 500;
-static WINDOW_WIDTH: u32 = 800;
+static WINDOW_HEIGHT: i32 = 500;
+static WINDOW_WIDTH: i32 = 800;
 static WINDOW_SHAPE: glutin::dpi::LogicalSize<f64> = glutin::dpi::LogicalSize::new(
     WINDOW_WIDTH as f64,
     WINDOW_HEIGHT as f64,
 );
+
+fn draw_gridline(display: &glium::Display, target: &mut glium::Frame, from_x: f32, from_y: f32, to_x: f32, to_y: f32) {
+    let shape = vec![
+        Vertex { position: [from_x, from_y] },
+        Vertex { position: [to_x, to_y] },
+    ];
+
+    let vertex_buffer = glium::VertexBuffer::new(display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
+
+    let vertex_shader_src = "
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    ";
+
+    let fragment_shader_src = format!("
+        #version 140
+        out vec4 color;
+        void main() {{
+            color = vec4({}, {}, {}, 1.0);
+        }}
+    ", GRIDLINES.red, GRIDLINES.green, GRIDLINES.blue);
+
+    let program = glium::Program::from_source(display, vertex_shader_src, &fragment_shader_src, None).unwrap();
+
+    (*target).draw( 
+        &vertex_buffer,
+        &indices,
+        &program,
+        &glium::uniforms::EmptyUniforms,
+        &Default::default()
+    ).unwrap();
+}
 
 impl Display {
     pub fn new(columns: i32, rows: i32) -> Display {
@@ -33,8 +69,6 @@ impl Display {
     }
 
     pub fn draw(&self, state: Vec<Vec<bool>>) {
-        // use this as a guide: https://github.com/glium/glium/blob/master/book/tuto-01-getting-started.md
-        
         let mut event_loop = glutin::event_loop::EventLoop::new();
         let wb = glutin::window::WindowBuilder::new();
         let cb = glutin::ContextBuilder::new();
@@ -43,34 +77,19 @@ impl Display {
         display.gl_window().window().set_title(WINDOW_TITLE);
         display.gl_window().window().set_inner_size(WINDOW_SHAPE);
 
-        let shape = vec![
-            Vertex { position: [0.0, 1.0] },
-            // Vertex { position: [one_pixel_width(), 1.0] },
-            // Vertex { position: [one_pixel_width(), -1.0] },
-            Vertex { position: [0.0, -1.0] },
-        ];
+        let mut vertical_gridline_positions = vec![];
+        for x in 0..self.columns {
+            vertical_gridline_positions.push(
+                2.0 * x as f32 / self.columns as f32 - 1.0
+            );
+        }
 
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-        let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
-        // Need to do some nasty-looking openGL stuff. Abstract away once it's working
-
-		let vertex_shader_src = "
-			#version 140
-			in vec2 position;
-			void main() {
-				gl_Position = vec4(position, 0.0, 1.0);
-			}
-		";
-
-		let fragment_shader_src = format!("
-			#version 140
-			out vec4 color;
-			void main() {{
-				color = vec4({}, {}, {}, 1.0);
-			}}
-		", GRIDLINES.red, GRIDLINES.green, GRIDLINES.blue);
-
-        let program = glium::Program::from_source(&display, vertex_shader_src, &fragment_shader_src, None).unwrap();
+        let mut horizontal_gridline_positions = vec![];
+        for y in 0..self.rows {
+            horizontal_gridline_positions.push(
+                2.0 * y as f32 / self.rows as f32 - 1.0
+            );
+        }
 
 		event_loop.run(move |ev, _, control_flow| {
 			let next_frame_time = std::time::Instant::now() +
@@ -96,14 +115,13 @@ impl Display {
                 1.0,
             );
 
-            target.draw( 
-                &vertex_buffer,
-                &indices,
-                &program,
-                &glium::uniforms::EmptyUniforms,
-                &Default::default()
-            ).unwrap();
-
+            for x in &vertical_gridline_positions {
+                draw_gridline(&display, &mut target, *x, 1.0, *x, -1.0);
+            }
+            for y in &horizontal_gridline_positions {
+                draw_gridline(&display, &mut target, 1.0, *y, -1.0, *y);
+            }
+            
             target.finish().unwrap();
 		});
     }
